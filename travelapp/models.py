40ac198aee56eb9f23e23cb735393b5395736a60
aswin <aswinpdas83@gmail.com>
2022-06-02 14:50:14 +0530

    small changes

--- travelapp/models.py
+++ travelapp/models.py
@@ -5,3 +5,5 @@ class place(models.Models):
     name=models.CharField(max_length=50)
     img=models.ImageField(upload_to='picture')
     desc=models.TextField()
+    price=models.IntegerField()
+    offer=models.BooleanField(default=False)
