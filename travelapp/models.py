d2e04ef9bc5735fb9dba2e7acbfaeddc4e2383a8
aswin <aswinpdas83@gmail.com>
2022-06-02 15:16:33 +0530

    small changes

--- travelapp/models.py
+++ travelapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class place(models.Models):
+class place(models.Model):
     name=models.CharField(max_length=50)
     img=models.ImageField(upload_to='picture')
     desc=models.TextField()
