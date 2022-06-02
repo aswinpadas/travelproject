13506cbd89c3a0663f006b2707a350a0a23fd9e7
aswin <aswinpdas83@gmail.com>
2022-06-02 14:48:05 +0530

    db update and other changes

--- travelapp/models.py
+++ travelapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class place(models.Models):
+    name=models.CharField(max_length=50)
+    img=models.ImageField(upload_to='picture')
+    desc=models.TextField()
--- travelapp/views.py
+++ travelapp/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from . models import place
 # Create your views he
 def fun(req):
-    return render(req,"index.html")
+    obj=place()
+    obj.name='Kerala'
+    obj.price=100
+    obj.desc='God\'s Own country'
+
+    return render(req,"index.html",{'result':obj})
     #return HttpResponse("Hello World")
