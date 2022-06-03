f96577b9476e11e0dc342b477351c0196a4600e8
aswin <aswinpdas83@gmail.com>
2022-06-03 16:22:46 +0530

    csrf token error and multivalue error fixed

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,11 @@ def register(req):
     if req.method =="POST":
         first_name=req.POST["first_name"]
         last_name=req.POST["last_name"]
-        username=req.POST["user_name"]
+        username=req.POST["username"]
         password1=req.POST["password1"]
         password2=req.POST["password2"]
         email=req.POST["email"]
-        user=User.objects.create_user(first_name=first_name,last_name=last_name,username=username,passsword=password1,email=email)
+        user=User.objects.create_user(first_name=first_name,last_name=last_name,username=username,password=password1,email=email)
         user.save();
         print('user created')
         return redirect('/')
